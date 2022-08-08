4c3afdee186b03c12ec418f62f64c44fd31c5963
Clayton Horning <clayton.horning@colorado.edu>
2022-08-07 20:50:18 -0600

    Script outside of head

--- components/Head/index.js
+++ components/Head/index.js
@@ -3,7 +3,7 @@ import Script from "next/script";
 
 export default function CustomHead({ title }) {
   return (
-    <Head>
+    <>
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-Y83HSTY35R"
         strategy="afterInteractive"
@@ -17,10 +17,12 @@ export default function CustomHead({ title }) {
           gtag('config', 'G-Y83HSTY35R');
         `}
       </Script>
-      <title>{title}</title>
-      <meta property="og:title" content="Punchbowl Pressure Washing" />
-      <meta property="og:url" content="https://punchbowlpw.com" />
-      <meta property="og:image" content="/og-image.png" />
-    </Head>
+      <Head>
+        <title>{title}</title>
+        <meta property="og:title" content="Punchbowl Pressure Washing" />
+        <meta property="og:url" content="https://punchbowlpw.com" />
+        <meta property="og:image" content="/og-image.png" />
+      </Head>
+    </>
   );
 }
