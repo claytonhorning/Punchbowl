221f848aad9841b002e84b4216dfc339693b4ccb
Clayton Horning <clayton.horning@colorado.edu>
2022-08-07 20:44:40 -0600

    Google analytics

--- components/Head/index.js
+++ components/Head/index.js
@@ -1,8 +1,22 @@
 import Head from "next/head";
+import Script from "next/script";
 
 export default function CustomHead({ title }) {
   return (
     <Head>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-Y83HSTY35R"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-Y83HSTY35R');
+        `}
+      </Script>
       <title>{title}</title>
       <meta property="og:title" content="Punchbowl Pressure Washing" />
       <meta property="og:url" content="https://punchbowlpw.com" />
