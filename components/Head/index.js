14438b0439f0ef5ee4f9fb0cb1655f3e713971d1
Clayton Horning <clayton.horning@colorado.edu>
2022-08-09 10:16:52 -0600

    Remove messenger

--- components/Head/index.js
+++ components/Head/index.js
@@ -23,33 +23,6 @@ export default function CustomHead({ title }) {
         <meta property="og:url" content="https://punchbowlpw.com" />
         <meta property="og:image" content="/og-image.png" />
       </Head>
-
-      <Script id="fb-root">
-        {`
-       var chatbox = document.getElementById('fb-customer-chat');
-      chatbox.setAttribute("page_id", "111088115036607");
-      chatbox.setAttribute("attribution", "biz_inbox");
-      `}
-      </Script>
-
-      <Script id="fb-customer-chat" className="fb-customerchat">
-        {`
-      window.fbAsyncInit = function() {
-        FB.init({
-          xfbml            : true,
-          version          : 'v14.0'
-        });
-      };
-
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-    `}
-      </Script>
     </>
   );
 }
